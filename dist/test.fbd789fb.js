de36bcc124b5923fa0cc8addca76904008e67d72
vanessacai <1668576526@qq.com>
2025-03-09 18:01:27 +0800

    update

--- dist/test.fbd789fb.js
+++ dist/test.fbd789fb.js
@@ -217,7 +217,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = '' || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + '40569' + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + '42009' + '/');
   ws.onmessage = function (event) {
     var data = JSON.parse(event.data);
 
@@ -359,4 +359,4 @@ function hmrAccept(bundle, id) {
   });
 }
 },{}]},{},["C:\\Users\\jingjinglol\\AppData\\Local\\Yarn\\Data\\global\\node_modules\\parcel\\src\\builtins\\hmr-runtime.js","test.js"], null)
-//# sourceMappingURL=/test.fbd789fb.map
+//# sourceMappingURL=test.fbd789fb.map
